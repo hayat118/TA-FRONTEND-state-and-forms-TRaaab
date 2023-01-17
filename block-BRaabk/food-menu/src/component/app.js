0de775c828ca955c7f2d657ca1e6cb55de4a28c8
ravi11o <ravi.suraj110@gmail.com>
2023-01-17 22:17:20 +0530

    food-menu

---
+++ block-BRaabk/food-menu/src/component/app.js
@@ -0,0 +1,14 @@
+import Food from '../component/food';
+import Header from '../component/header';
+
+import '../styles/index.css'
+
+function App(){
+  return(
+    <>
+     <Header />
+     <Food />
+    </>
+  )
+}
+export default App;
---
+++ block-BRaabk/food-menu/src/component/header.js
@@ -0,0 +1,9 @@
+
+function Header(){
+  return(
+    <>
+    <h1>Our Menu</h1><hr />
+    </>
+  )
+}
+export default Header
